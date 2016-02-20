00dddd6ab8a6e32b39580b0229d8b5ebddf2288f
Liubo <1003670604@qq.com>
2016-02-20 17:34:29 +0800

    Liu

--- Prepare/js/index.js
+++ Prepare/js/index.js
@@ -3,15 +3,14 @@
  */
 
 $(function(){
-<<<<<<< HEAD
     $('#dowebok').fullpage({
         sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#f09', '#f09', '#f09'],
         anchors: ['page1', 'page2', 'page3', 'page4','page5','page6','page7'],
-=======
+/*=======
 	 $('#dowebok').fullpage({
         sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#f09'],
         anchors: ['page1', 'page2', 'page3', 'page4','page5'],
->>>>>>> 42f2ca80aa98102b769bcd35ac143252047aeb84
+>>>>>>> 42f2ca80aa98102b769bcd35ac143252047aeb84*/
         menu: '#menu'
     });
 
