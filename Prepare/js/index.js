312d87fa35e22a0dd20dc0f4daf0613c4c37df65
Liubo <1003670604@qq.com>
2016-02-20 17:01:21 +0800

    Liu

--- Prepare/js/index.js
+++ Prepare/js/index.js
@@ -4,8 +4,8 @@
 
 $(function(){
     $('#dowebok').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#f09'],
-        anchors: ['page1', 'page2', 'page3', 'page4','page5'],
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#f09', '#f09', '#f09'],
+        anchors: ['page1', 'page2', 'page3', 'page4','page5','page6','page7'],
         menu: '#menu'
     });
 });
